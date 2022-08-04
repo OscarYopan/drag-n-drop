4556630e6b3d7636d610bbe077ceb01df15a5f6f
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-04 16:26:12 -0300

    --

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
 
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties) {
+  empty.addEventListener('dragover', dragOver)
+  empty.addEventListener('dragenter', dragEnter)
+  empty.addEventListener('dragleave', dragLeave)
+  empty.addEventListener('drop', dragDrop)
+}
 
 
 function dragStart () {
-
+  console.log('start');
 }
 
 function dragEnd () {
-  
+  console.log('end');
 }
 
 function dragEnter() {
-  
+  console.log('enter');
 }
 
-function dragStart () {
-  
+function dragOver () {
+  console.log('Over');
+}
+
+function dragLeave () {
+ console.log('leave'); 
+}
+
+function dragDrop () {
+  console.log('drop');
 }
